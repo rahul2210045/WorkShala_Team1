d99edd550ccc0a4729716ee2ebb093c09100a8a5
yadavrahul818980 <144869056+yadavrahul818980@users.noreply.github.com>
2023-11-20 02:30:31 +0530

    company data api fetching

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 255, 255, 255)),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: SplashScreen(),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
